ec5a031da5642dc21c0c2b325a7f1bc3cadc1e3f
o-diaz <o-diaz@smalltalk.co.jp>
2012-12-12 18:32:35 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Porlosegao::Application.config.secret_token = '8622999a3aea4fb6a0887c2d16761d677de3d200c328580b1730b8c72feff1436e679d53613b78427a3f42281c16a3ed804b6ae37845b8c7f3f7bff42f282940'
