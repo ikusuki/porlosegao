83f78063cc855f3f6172ec48396ac0053ff87074
o-diaz <o-diaz@smalltalk.co.jp>
2012-12-15 16:51:01 +0900

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  http_basic_authenticate_with :name => "bythesegao", :password => "istheway", :except => :keep_alive
 end
