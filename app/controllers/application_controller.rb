512aeeb28ab19b599c4577dece61961b46957ba1
o-diaz <o-diaz@smalltalk.co.jp>
2012-12-18 14:54:08 +0900

    New auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  http_basic_authenticate_with :name => "bythesegao", :password => "istheway", :except => :keep_alive
+  http_basic_authenticate_with :name => "tosca", :password => "koki142", :except => :keep_alive
 end
