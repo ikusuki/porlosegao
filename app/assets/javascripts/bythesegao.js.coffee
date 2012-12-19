80ec92a024fd2803caf2e3b01e35f591c5f8f747
o-diaz <o-diaz@smalltalk.co.jp>
2012-12-19 14:09:03 +0900

    authentication via email

--- app/assets/javascripts/bythesegao.js.coffee
+++ app/assets/javascripts/bythesegao.js.coffee
@@ -1,2 +1,29 @@
 $ ->
-  $('#signin_form').validationEngine();
+  $('#signin_form, #login_form').validationEngine();
+  window.myMessages = ['info','warning','error','success']
+  # Initially, hide them all
+  hideAllMessages()
+
+
+  showMessage 'info'
+
+
+  # When message is clicked, hide it
+  $(".message").click ->
+    $(this).animate
+      top: -$(this).outerHeight()
+    , 500
+
+hideAllMessages = () ->
+  messagesHeights = new Array() # this array will store height for each
+  i = 0
+  while i < window.myMessages.length
+    messagesHeights[i] = $("." + window.myMessages[i]).outerHeight() # fill array
+    $("." + window.myMessages[i]).css "top", -messagesHeights[i] #move element outside viewport
+    i++
+  true
+
+showMessage = (type) ->
+  $("." + type).animate
+    top: "0"
+  , 500
--- vendor/assets/javascripts/jquery.validationEngine-es.js
+++ vendor/assets/javascripts/jquery.validationEngine-es.js
@@ -2,12 +2,14 @@
 (function($){
     $.fn.validationEngineLanguage = function(){
     };
+    motes = Array("Muchacho", "Chacho", "Pataliebre", "Artista");
+    function getMote() {        return motes[Math.floor(Math.random()*motes.length)];    }
     $.validationEngineLanguage = {
         newLang: function(){
             $.validationEngineLanguage.allRules = {
                 "required": { // Add your regex rules here, you can take telephone as an example
                     "regex": "none",
-                    "alertText": "* Este campo es obligatorio",
+                    "alertText": "* " + getMote() + ", mete esto!",
                     "alertTextCheckboxMultiple": "* Por favor seleccione una opción",
                     "alertTextCheckboxe": "* Este checkbox es obligatorio"
                 },
@@ -19,8 +21,8 @@
                 },
                 "minSize": {
                     "regex": "none",
-                    "alertText": "* Mínimo de ",
-                    "alertText2": " caracteres autorizados"
+                    "alertText": "* Dale por lo menos ",
+                    "alertText2": " letras"
                 },
 				"groupRequired": {
                     "regex": "none",
@@ -58,7 +60,7 @@
                 },
                 "equals": {
                     "regex": "none",
-                    "alertText": "* Los campos no coinciden"
+                    "alertText": "* Muchachooo, no cuadran!"
                 },
                 "creditCard": {
                     "regex": "none",
@@ -72,7 +74,7 @@
                 "email": {
                     // Shamelessly lifted from Scott Gonzalez via the Bassistance Validation plugin http://projects.scottsplayground.com/email_address_validation/
                     "regex": /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
-                    "alertText": "* Correo inválido"
+                    "alertText": "* Email raruno detected"
                 },
                 "integer": {
                     "regex": /^[\-\+]?\d+$/,
