992dccaec2d520a189a33f3701b36c5cd6be6171
o-diaz <o-diaz@smalltalk.co.jp>
2012-12-20 11:17:28 +0900

    normal authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  http_basic_authenticate_with :name => "tosca", :password => "koki142", :except => :keep_alive
+  http_basic_authenticate_with :name => "bythesegao", :password => "istheway", :except => :keep_alive
 
   def after_sign_in_path_for(resource_or_scope)
     session[:just_login] = true 
