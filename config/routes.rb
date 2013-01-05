3aa8c4555ccfd14bad9398ee6d0a2fe33390e817
Koki <koki142@gmail.com>
2013-01-05 12:11:13 +0900

    root to nuevos

--- config/routes.rb
+++ config/routes.rb
@@ -78,7 +78,7 @@ Porlosegao::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'cards#index'
+  root :to => 'cards#nuevos'
 
   # See how all your routes lay out with "rake routes"
 
