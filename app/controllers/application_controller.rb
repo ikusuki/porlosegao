bd9e1fb4c8acb94b269d958146781224ca87347d
Koki <koki142@gmail.com>
2013-01-06 18:46:23 +0900

    Mobile layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   # http_basic_authenticate_with :name => "bythesegao", :password => "istheway", :except => :keep_alive
+  layout :which_layout
+  
+  def which_layout
+    @mobile = detect_mobile_browser
+    @mobile ? "mobile" : "application"
+  end
 
   def after_sign_in_path_for(resource_or_scope)
     session[:just_login] = true 
@@ -12,4 +18,18 @@ class ApplicationController < ActionController::Base
     session[:just_register] = true
     root_path
   end
+
+  private
+
+  MOBILE_BROWSERS = ["playbook", "windows phone", "android", "ipod", "iphone", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
+
+  def detect_mobile_browser
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+  
+    MOBILE_BROWSERS.each do |m|
+      return true if agent.match(m)
+    end
+    return false
+  end
+
 end
