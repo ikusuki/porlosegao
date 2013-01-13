0d9b61e9a2664b9ef33fc61a38464dfa4b825f77
Koki <koki142@gmail.com>
2013-01-13 15:58:39 +0900

    mobile to index in PC

--- app/assets/javascripts/cromoengine.js.coffee
+++ app/assets/javascripts/cromoengine.js.coffee
@@ -10,6 +10,7 @@ $ ->
   handler.toscamark(cromoptions)
 
   $('.cromo').live('mouseover', ->
+    $('ul.social', $(this)).show()
     $('.credits', $(this)).stop().show().animate
       height: '15px'
       top: '-20px'
@@ -18,6 +19,7 @@ $ ->
       left: '0px'
     , 100
   ).live 'mouseout', ->
+    $('ul.social', $(this)).hide()
     $credits = $('.credits', $(this))
     unless $credits.hasClass('hover')
       $credits.stop().animate
--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,9 +1,27 @@
+# -*- coding: utf-8 -*- 
 module CardsHelper
 
   def vote_class(card_id)
     return nil if current_user.blank?
     return "clicked" if !@vote.blank?
     return "clicked" if !@votes.blank? && @votes.include?(card_id)
+  end
+
+  def twitter_link(card)
+    link = "https://twitter.com/intent/tweet?text=Ojo al cromo!! "
+    link += "「#{card.picture.description}」" unless card.picture.description.blank?
+    link += card_url(card)
+  end
+
+  def facebook_link(card)
+    link = "https://facebook.com/sharer/sharer.php?u=#{card_url(card)}&t=Ojo al cromo!! "
+    link += "「#{card.picture.description}」" unless card.picture.description.blank?
+  end
+
+  def line_link(card)
+    link = "line://msg/text/Ojo al cromo!! "
+    link += "「#{card.picture.description}」" unless card.picture.description.blank?
+    link += card_url(card)
   end
 
 end
